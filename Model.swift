446903884dcbe386e2ad35a20bbd177102d27a67
Maksytaliev Shaiyr <73332395+Shaiyr-MS@users.noreply.github.com>
2020-10-29 19:16:13 +0600

    Add files via upload

---
+++ Model.swift
@@ -0,0 +1,28 @@
+//
+//  Model.swift
+//  Notes
+//
+//  Created by Shaiyr on 10/29/20.
+//
+//
+//
+//import Foundation
+//
+////var articles: [Article] = []
+//
+//func loadNews() {
+//    _ = URL (string: "http,://jsonplaceholder.typicode.com/posts")
+//    let sension = URLSession(configuration: .default)
+//
+//    let downloadTask = URLSession.shared.downloadTask(with: URL) { (urlFile, responce, error) in
+//        if urlFile != nil {
+//        let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
+//        let urlPath = URL(fileURLWithPath: path)
+//           FileManager.default.copyItem(at: urlFile!, to: urlPath)
+//
+//            print(urlPath)
+//        }
+//    }
+//
+//    downloadTask.resume()
+//}
